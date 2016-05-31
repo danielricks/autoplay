d416fc97b163578ab375fb5790b35bced0917ccf
Daniel Ricks <daniel.ricks4@gmail.com>
2016-05-31 17:23:36 -0600

    Added gitignore file and setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/kingjamesiv/textplayer.git
+cd textplayer/
+git clone https://github.com/kingjamesiv/frotz.git
+cd frotz/
+make dumb
+cd ../..
+git clone https://github.com/kingjamesiv/scholar.git
+git clone https://github.com/kingjamesiv/conceptnetter.git
