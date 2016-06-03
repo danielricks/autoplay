56b005565c958439c3089136f87b197d0ef7cace
kingjamesiv <daniel.ricks2@gmail.com>
2016-06-03 11:02:53 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,20 @@
+# Install TextPlayer
 git clone https://github.com/kingjamesiv/textplayer.git
+
+# Install a modified fork of Frotz and build it specifically for interaction with the command line
 cd textplayer/
 git clone https://github.com/kingjamesiv/frotz.git
 cd frotz/
 make dumb
 cd ../..
+
+# Install Scholar
 git clone https://github.com/kingjamesiv/scholar.git
+
+# Install Conceptnetter
 git clone https://github.com/kingjamesiv/conceptnetter.git
+
+# Install NLTK and Word2Vec
+pip install nltk
+sudo apt-get -y install cython
+sudo pip install word2vec
