0c947a7bf54024f0791fb40561b6c1398838e5cd
Daniel Ricks <danielricks@users.noreply.github.com>
2017-03-07 12:52:01 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
+
+# Install TextPlayer
+git clone https://github.com/kingjamesiv/textplayer.git
+
 # Install a modified fork of Frotz and build it specifically for interaction with the command line
 cd textplayer/
 git clone https://github.com/kingjamesiv/frotz.git
 cd frotz/
 make dumb
 cd ../..
-
-# Install TextPlayer
-git clone https://github.com/kingjamesiv/textplayer.git
 
 # Install NLTK and Word2Vec (Requires pip)
 sudo apt-get -y install pip
